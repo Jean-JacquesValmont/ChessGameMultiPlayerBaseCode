882fd4fac5b953dbcc04ea085c60c7ee0d056513
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-11-20 14:04:13 +0100

    Connexion fonctionnel et dernière modification sur le Online pour les variables.

--- Script/NakamaOnline/Online.gd
+++ Script/NakamaOnline/Online.gd
@@ -36,9 +36,9 @@ var nakama_socket: NakamaSocket:
 # Internal variable for initializing the socket.
 var _nakama_socket_connecting := false
 
-signal session_changed (nakama_session)
-signal session_connected (nakama_session)
-signal socket_connected (nakama_socket)
+signal session_changed (_nakama_session)
+signal session_connected (_nakama_session)
+signal socket_connected (_nakama_socket)
 
 func _set_readonly_variable(_value) -> void:
 	pass
@@ -68,24 +68,24 @@ func set_nakama_session(_nakama_session: NakamaSession) -> void:
 	
 	__nakama_session = _nakama_session
 	
-	emit_signal("session_changed", nakama_session)
+	emit_signal("session_changed", __nakama_session)
 	
-	if nakama_session and not nakama_session.is_exception() and not nakama_session.is_expired():
-		emit_signal("session_connected", nakama_session)
+	if __nakama_session and not __nakama_session.is_exception() and not __nakama_session.is_expired():
+		emit_signal("session_connected", __nakama_session)
 
 func connect_nakama_socket() -> void:
-	if nakama_socket != null:
+	if _nakama_socket != null:
 		return
 	if _nakama_socket_connecting:
 		return
 	_nakama_socket_connecting = true
 	
-	var new_socket = Nakama.create_socket_from(nakama_client)
-	await new_socket.connect_async(nakama_session)
-	nakama_socket = new_socket
+	var new_socket = Nakama.create_socket_from(_nakama_client)
+	await new_socket.connect_async(__nakama_session)
+	_nakama_socket = new_socket
 	_nakama_socket_connecting = false
 	
-	emit_signal("socket_connected", nakama_socket)
+	emit_signal("socket_connected", _nakama_socket)
 
 func is_nakama_socket_connected() -> bool:
-	return nakama_socket != null && nakama_socket.is_connected_to_host()
+	return _nakama_socket != null && _nakama_socket.is_connected_to_host()
